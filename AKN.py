2f23dc9b00d15458e86fe0384ac12d7cca71d427
Fazlaalahi <110632916+Fazlaalahi@users.noreply.github.com>
2023-11-04 00:43:37 +0600

    Add files via upload

--- AKN.py
+++ AKN.py
@@ -80,7 +80,7 @@ def login():
         clear()
         cookies = input(' Put cookies: ')
         try:
-                data = requests.get("https://business.facebook.com/business_locations", headers = {"authority': 'x.facebook.com',
+                data = requests.get("https://business.facebook.com/business_locations", headers = {"authority':'x.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
